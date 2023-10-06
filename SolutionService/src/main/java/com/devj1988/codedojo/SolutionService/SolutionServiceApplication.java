2894211e3c971672588120dc5a7c8718d8759a2c
Dev Jyoti Behera <devj1988@gmail.com>
2023-10-06 15:31:00 -0500

    added SolutionService

---
+++ SolutionService/src/main/java/com/devj1988/codedojo/SolutionService/SolutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devj1988.codedojo.SolutionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionServiceApplication.class, args);
+	}
+
+}
---
+++ SolutionService/src/test/java/com/devj1988/codedojo/SolutionService/SolutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devj1988.codedojo.SolutionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
