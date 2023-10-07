4863edafeb16e26a2066538bdb67411186d284a0
Dev Jyoti Behera <devj1988@gmail.com>
2023-10-07 12:44:03 -0500

    updated Python driver to add error stacktrace

--- SolutionService/drivers/pythonDriver.py
+++ SolutionService/drivers/pythonDriver.py
@@ -1,4 +1,4 @@
-import sys, json, importlib, os
+import sys, json, importlib, os, traceback
 
 def logDriverMessage(msg):
     print(f"DRIVER_MSG {msg}")
@@ -13,16 +13,53 @@ def getSolutionObj(usercodeModuleName):
     solutionClass = getattr(module, 'Solution')
     return solutionClass()
 
-def runTests(obj, testSpecs):
+def runTests(obj, testSpecs, exitOnError=True):
     func = getattr(obj, testSpecs['entryPoint'])
+    summary = {
+        "total": len(testSpecs['testcases']),
+        "passed": 0,
+        "failed": 0,
+        "run": 0
+    }
     for i, case in enumerate(testSpecs['testcases']):
         inp = case["input"]
         logDriverMessage(f"Test case {i+1} running")
-        out = func(*inp)
+        try:
+            summary["run"] += 1
+            out = func(*inp)
+        except Exception as e:
+            summary["failed"] += 1
+            formatted_lines = traceback.format_exc().splitlines()
+            logDriverMessage(json.dumps({
+                "case": i+1,
+                "result": "FAIL",
+                "details": {
+                    "error_trace": formatted_lines[-1:-10:-1]
+                }
+            }))
+            if exitOnError:
+                break
+            else:
+                continue
         if out == case["output"]:
-            logDriverMessage(f"Test case {i+1} passed")
+            logDriverMessage(json.dumps({
+                "case": i+1,
+                "result": "PASS"
+            }))
+            summary["passed"] += 1
         else:
-            logDriverMessage(f"Test case {i+1} failed")
+            logDriverMessage(json.dumps({
+                "case": i+1,
+                "result": "FAIL",
+                "details": {
+                    "input_args": str(inp),
+                    "expected": case["output"],
+                    "actual": out
+                }
+            }))
+            summary["failed"] += 1
+    return json.dumps({"summary": summary})
+
 def main():
     usercodeModuleName = sys.argv[1]
     testSpecFile = sys.argv[2]
@@ -31,7 +68,14 @@ def main():
     if not hasattr(solutionObj, testSpecs['entryPoint']):
         logDriverMessage("entrypoint not found")
         sys.exit(1)
-    runTests(solutionObj, testSpecs)
+    ret = {"exit": True, "error": False}
+    try:
+        summary = runTests(solutionObj, testSpecs)
+        logDriverMessage(summary)
+    except Exception as e:
+        ret["error"] = True
+    logDriverMessage(json.dumps(ret))
+
 
 
 main()
